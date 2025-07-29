3c4e2e808bce5aa10bb4e37c564bb8aab1f19c2a
Victor Neves <victorneves478@gmail.com>
2025-07-29 11:30:52 -0300

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,25 +1,25 @@
 import HeroSection from "@/components/HeroSection";
-import BenefitsSection from "@/components/BenefitsSection";
-import AuthorSection from "@/components/AuthorSection";
-import ProductSection from "@/components/ProductSection";
-import BonusSection from "@/components/BonusSection";
-import TestimonialsSection from "@/components/TestimonialsSection";
-import OfferSection from "@/components/OfferSection";
-import FAQSection from "@/components/FAQSection";
-import Footer from "@/components/Footer";
+// import BenefitsSection from "@/components/BenefitsSection";
+// import AuthorSection from "@/components/AuthorSection";
+// import ProductSection from "@/components/ProductSection";
+// import BonusSection from "@/components/BonusSection";
+// import TestimonialsSection from "@/components/TestimonialsSection";
+// import OfferSection from "@/components/OfferSection";
+// import FAQSection from "@/components/FAQSection";
+// import Footer from "@/components/Footer";
 
 const Index = () => {
   return (
     <div className="min-h-screen font-poppins">
       <HeroSection />
-      <BenefitsSection />
-      <AuthorSection />
-      <ProductSection />
-      <BonusSection />
-      <TestimonialsSection />
-      <OfferSection />
-      <FAQSection />
-      <Footer />
+      {/* <BenefitsSection /> */}
+      {/* <AuthorSection /> */}
+      {/* <ProductSection /> */}
+      {/* <BonusSection /> */}
+      {/* <TestimonialsSection /> */}
+      {/* <OfferSection /> */}
+      {/* <FAQSection /> */}
+      {/* <Footer /> */}
     </div>
   );
 };
