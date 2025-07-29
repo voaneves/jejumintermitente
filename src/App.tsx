ff665d18670a97207ad3df7e6f1b651c0777e81f
Victor Neves <victorneves478@gmail.com>
2025-07-29 11:20:51 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
+import Author from "./components/author";
+import Benefits from "./components/benefits";
+import Bonus from "./components/bonus";
+import Faq from "./components/faq";
+import Footer from "./components/footer";
+import Guide from "./components/guide";
+import Header from "./components/header";
+import Offer from "./components/offer";
+import SocialProof from "./components/social-proof";
+import Warranty from "./components/warranty";
+
 function App() {
   return (
-    <div>
-      <h1>Teste de Deploy</h1>
-      <p>Se você está vendo esta mensagem, o deploy funcionou!</p>
-    </div>
+    <main className="flex flex-col">
+      <Header />
+      {/* <Benefits /> */}
+      {/* <Author /> */}
+      {/* <Guide /> */}
+      {/* <Bonus /> */}
+      {/* <SocialProof /> */}
+      {/* <Offer /> */}
+      {/* <Warranty /> */}
+      {/* <Faq /> */}
+      {/* <Footer /> */}
+    </main>
   );
 }
 
