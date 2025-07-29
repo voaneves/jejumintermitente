3613241e2e0e3298cbf928c82357e65c05de1f50
Victor Neves <victorneves478@gmail.com>
2025-07-29 10:47:06 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  // Adicione a linha base aqui
+  base: '/natural-flow-landing/',
+
   server: {
     host: "::",
     port: 8080,
