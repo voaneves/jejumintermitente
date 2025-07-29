1642dbe54eead0c6d0176af8b58f37f329477a89
Victor Neves <victorneves478@gmail.com>
2025-07-29 11:42:32 -0300

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,25 +1,26 @@
 import HeroSection from "@/components/HeroSection";
-// import BenefitsSection from "@/components/BenefitsSection";
-// import AuthorSection from "@/components/AuthorSection";
-// import ProductSection from "@/components/ProductSection";
-// import BonusSection from "@/components/BonusSection";
-// import TestimonialsSection from "@/components/TestimonialsSection";
-// import OfferSection from "@/components/OfferSection";
-// import FAQSection from "@/components/FAQSection";
-// import Footer from "@/components/Footer";
+import BenefitsSection from "@/components/BenefitsSection";
+import AuthorSection from "@/components/AuthorSection";
+import ProductSection from "@/components/ProductSection";
+import BonusSection from "@/components/BonusSection";
+import TestimonialsSection from "@/components/TestimonialsSection";
+import OfferSection from "@/components/OfferSection";
+import FAQSection from "@/components/FAQSection";
+import Footer from "@/components/Footer";
 
 const Index = () => {
   return (
-    <div className="min-h-screen font-poppins">
+    // Sugestão: adicionei um 'overflow-x-hidden' para evitar barras de rolagem horizontais indesejadas
+    <div className="min-h-screen font-poppins overflow-x-hidden">
       <HeroSection />
-      {/* <BenefitsSection /> */}
-      {/* <AuthorSection /> */}
-      {/* <ProductSection /> */}
-      {/* <BonusSection /> */}
-      {/* <TestimonialsSection /> */}
-      {/* <OfferSection /> */}
-      {/* <FAQSection /> */}
-      {/* <Footer /> */}
+      <BenefitsSection />
+      <AuthorSection />
+      <ProductSection />
+      <BonusSection />
+      <TestimonialsSection />
+      <OfferSection />
+      <FAQSection />
+      <Footer />
     </div>
   );
 };
