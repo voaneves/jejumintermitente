034a1770c7729e3d610cb5e9db170b5c565f58ae
Victor Neves <victorneves478@gmail.com>
2025-07-29 11:01:41 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,20 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
 import './index.css'
+// Altere o que você importa aqui
+import { createHashRouter, RouterProvider } from 'react-router-dom'
 
-createRoot(document.getElementById("root")!).render(<App />);
+// Altere a função e o path aqui
+const router = createHashRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
