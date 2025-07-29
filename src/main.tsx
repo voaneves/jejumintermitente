d83b872f4bacd7570b704f8fd731e1b6c8bdc7d8
Victor Neves <victorneves478@gmail.com>
2025-07-29 11:39:12 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,32 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-// Altere o que você importa aqui
-import { createHashRouter, RouterProvider } from 'react-router-dom'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createHashRouter, RouterProvider } from 'react-router-dom';
 
-// Altere a função e o path aqui
+import App from './App.tsx';
+import Index from './pages/Index';
+import NotFound from './pages/NotFound';
+import './index.css';
+
+// A definição de todas as rotas fica aqui
 const router = createHashRouter([
   {
-    path: "/",
-    element: <App />,
+    path: '/',
+    element: <App />, // App.tsx é o elemento "pai", o layout
+    children: [ // As páginas são "filhas" do layout
+      {
+        index: true, // A rota inicial
+        element: <Index />,
+      },
+      {
+        path: '*', // Rota para "não encontrado"
+        element: <NotFound />,
+      },
+    ],
   },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>,
-)
+);
